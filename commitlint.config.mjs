db5343a53ddaf62e01edca1a5f7f3e2c60a81045
Franz Evangelista <franz.evangelista@timberyardcommerce.com>
2026-04-20 15:16:25 +0800

    chore: simplify commitlint configuration by removing unused parser options and rules

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -1,14 +1,3 @@
 export default {
-  extends: ['@commitlint/config-conventional'],
-  defaultIgnores: true,
-  parserPreset: {
-    parserOpts: {
-      headerPattern:
-        /^(build|chore|ci|docs|feat|fix|perf|refactor|revert|style|test)(?:\(([A-Z][A-Z0-9]*-\d+)\))?!?: (.+)$/,
-      headerCorrespondence: ['type', 'scope', 'subject'],
-    },
-  },
-  rules: {
-    'scope-empty': [2, 'never'],
-  },
+  extends: ['@commitlint/config-conventional'], // or => @timberyard/commitlint-config
 }
